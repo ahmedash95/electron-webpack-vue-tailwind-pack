ae73cf821b4cca1077f2582bf246c643986bb444
Ahmed Ashraf <ahmed29329@gmail.com>
2020-05-15 11:19:15 +0200

    add devtron

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -4,7 +4,9 @@ import Vue from "vue"
 
 Vue.config.productionTip = false
 Vue.config.devtools = process.env.NODE_ENV === "development"
-
+if(process.env.NODE_ENV === "development") {
+  require('devtron').install()
+}
 
 import App from './App.vue'
 new Vue({
