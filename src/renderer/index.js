09fbc172a50b6fbf13a876b59fb324dc416367e0
Ahmed Ashraf <ahmed29329@gmail.com>
2020-05-16 15:06:02 +0200

    add vue-router

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,6 +1,9 @@
 import './styles.scss';
 
 import Vue from "vue"
+import VueRouter from "vue-router"
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 Vue.config.devtools = process.env.NODE_ENV === "development"
@@ -8,7 +11,16 @@ if(process.env.NODE_ENV === "development") {
   require('devtron').install()
 }
 
+const Foo = { template: '<div class="font-bold text-center w-full text-gray-800 text-2xl mt-10">Hello World</div>' }
+const routes = [
+  { path: '/', component: Foo },
+]
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
 import App from './App.vue'
 new Vue({
-  render: h => h(App)
+  // render: h => h(App)
+  router
 }).$mount('#app')
--- webpack.renderer.additions.js
+++ webpack.renderer.additions.js
@@ -1,4 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({ //
+      title: "Tabur",
+      template: "src/renderer/index.html",
+      hash: true
+    }),
+  ],
   module: {
     rules: [
       {
